5979c7f01f4a5bb0a99205ed67bead43a86fe8b9
Faidon Liambotis <faidon@wikimedia.org>
2020-02-27 04:05:05 +0200

    Add a rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Package configuration."""
+# fmt: off
+
+import setuptools
+
+
+setuptools.setup(
+    name="ircstream",
+    py_modules=["ircstream"],
+    maintainer="Faidon Liambotis",
+    maintainer_email="faidon@wikimedia.org",
+    description="Wikimedia RC->IRC gateway",
+    url="https://github.com/wikimedia/ircstream",
+    license="Apache2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: System Administrators",
+    ],
+    keywords=["irc", "wikimedia"],
+    python_requires=">=3.7",
+    install_requires=[
+        "prometheus_client",
+        "structlog",
+    ],
+    setup_requires=[
+        "setuptools_scm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ircstream = ircstream:main",
+        ],
+    },
+    zip_safe=False,
+    use_scm_version=True,
+)
