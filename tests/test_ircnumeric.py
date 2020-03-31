428de2a4467a0da1f0972fb594304d986156d988
Faidon Liambotis <faidon@wikimedia.org>
2020-03-31 05:39:24 +0300

    tests: add tests for IRCNumeric
    The IRCNumeric classes are simple Enums, so interface-wise there isn't
    much to test. However, we can always test that our numerics are valid:
    test against the irc-defs repository's numerics YAML file, to make sure
    that we have the right name<->numeric mapping.

---
+++ tests/test_ircnumeric.py
@@ -0,0 +1,37 @@
+"""Test the IRCNumeric class, including RPL and ERR."""
+
+from pathlib import Path
+from typing import Dict, Set
+
+from ircstream import ERR, RPL
+
+import yaml
+
+TEST_NUMERICS_YAML = Path("testdata") / "numerics.yaml"
+
+
+def test_numeric() -> None:
+    """Test RPL and ERR (and their base class, IRCNumeric) against irc-defs.
+
+    This checks that the names and values match the one from
+    https://github.com/ircdocs/irc-defs/
+
+    Our list is not exhaustive, nor it tries to be, so we only check that our
+    definitions are valid, and not that we have the complete list.
+    """
+    # build a name->(numeric, numeric, ...) dictionary, e.g. RPL_WELCOME -> (001,)
+    yamldata = yaml.safe_load(open(TEST_NUMERICS_YAML).read())
+    numerics: Dict[str, Set[str]] = {}
+    for value in yamldata["values"]:
+        # numerics are not necessarily unique and vary by implementation.
+        # e.g. RPL_WHOISHOST is both 378 and 616, and 378 can also be RPL_BANEXPIRED
+        #
+        # collect the set of all those values.
+        name, numeric = value["name"], value["numeric"]
+        if name not in numerics:
+            numerics[name] = {numeric}
+        else:
+            numerics[name].add(numeric)
+
+    for value in (*RPL, *ERR):
+        assert str(value) in numerics[repr(value)]
