64a5d984f37f638201605d3697daa02da5b249f2
Faidon Liambotis <faidon@wikimedia.org>
2020-04-03 22:04:36 +0300

    tests: increase test coverage of configure_logging
    Add separate tests for each logging format, to make sure that all of
    them work.

--- tests/test_argparse_misc.py
+++ tests/test_argparse_misc.py
@@ -31,13 +31,26 @@ def test_config_nonexistent(capsys):
 
 
 def test_configure_logging_plain(caplog):
-    """Test that logging configuration works."""
+    """Test that the plain logging configuration works."""
     ircstream.configure_logging("DEBUG", "plain")
     log = structlog.get_logger("testlogger")
     caplog.clear()
     log.warn("this is a test log")
     assert ["this is a test log"] == [rec.message for rec in caplog.records]
 
+
+def test_configure_logging_console(caplog):
+    """Test that the console logging configuration works."""
+    ircstream.configure_logging("DEBUG", "plain")  # needed to override the default noop config in testing
+    ircstream.configure_logging("DEBUG", "console")
+    log = structlog.get_logger("testlogger")
+    caplog.clear()
+    log.warn("this is a test log")
+    assert ["[warning  ] this is a test log"] == [rec.message for rec in caplog.records]
+
+
+def test_configure_logging_json(caplog):
+    """Test that the json logging configuration works."""
     ircstream.configure_logging("DEBUG", "json")
     log = structlog.get_logger("testlogger")
     caplog.clear()
@@ -46,3 +59,9 @@ def test_configure_logging_plain(caplog):
     parsed_logs = [json.loads(rec.message) for rec in caplog.records]
     assert ["this is a json log"] == [rec["event"] for rec in parsed_logs]
     assert ["value"] == [rec["key"] for rec in parsed_logs]
+
+
+def test_configure_logging_invalid():
+    """Test that an invalid logging configuration does not work."""
+    with pytest.raises(ValueError):
+        ircstream.configure_logging("DEBUG", "invalid")
